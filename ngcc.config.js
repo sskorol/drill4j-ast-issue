8d3288f6ff49171f25ed2ca17432b279122a02e4
sskorol <serhii.s.korol@gmail.com>
2021-11-18 18:59:44 +0200

    Added configs to reproduce coverage issue.

---
+++ ngcc.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  packages: {
+    'ngx-currency-mask': {
+      entryPoints: {
+        '.': {
+          override: {
+            main: 'index.js',
+          },
+        },
+      },
+    },
+    'ng2-google-charts': {
+      entryPoints: {
+        '.': {
+          override: {
+            main: 'index.js',
+          },
+        },
+      },
+    },
+    'angular2-multiselect-dropdown': {
+      entryPoints: {
+        '.': {
+          override: {
+            main: 'angular2-multiselect-dropdown.js',
+          },
+        },
+      },
+    },
+  },
+};
